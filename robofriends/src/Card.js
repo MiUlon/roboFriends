bcf6654ae53e8e53e5107d0d067ff8008bb1cca1
Mindaugas Ulonas <mindegi15@gmail.com>
2022-09-09 16:34:53 +0300

    adding all robots

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ id, name, email }) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='Robot icon' src={`https://robohash.org/${id}?size=200x200`}></img>
+            <img alt='Robot icon' src={`https://robohash.org/${id}`}></img>
             <h2>{name}</h2>
             <p>{email}</p>
         </div>
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
 import 'tachyons';
 import { robots } from './robots';
 import CardList from './CardList';
@@ -10,7 +9,7 @@ import CardList from './CardList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList />
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
 
